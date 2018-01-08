8ed7c9a1bbe89c32ceaaa9a2263815ab2b08cc1d
Jonathon Copeland <jcopela4@gmail.com>
2018-01-08 14:06:21 -0500

    added auto build scripts, all tmp files are just there to keep directory structure in git

---
+++ scripts/compile.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+start_dir=$(pwd)
+echo "compiling in "$(pwd)
+javac -s bin -d bin -cp lib/libTUIO.jar src/com/crunchy/MouseDriver/TuioMouse.java
+cd bin
+jar xf ../lib/libTUIO.jar
+rm -r META-INF
+cd ../
+echo "jaring in "$(pwd)
+jar cvmf $start_dir/MANIFEST.MF $start_dir/out/TuioMouse.jar -C bin .
+
