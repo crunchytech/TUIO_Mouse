47c1a4ec92dbcb749234bbb695749c0310d6f345
Jonathon Copeland <jcopela4@gmail.com>
2018-03-12 10:51:49 -0400

    fixed timeput issue and added log levels

--- TUIO_Mouse/src/com/crunchy/MouseDriver/TuioMouse.java
+++ TUIO_Mouse/src/com/crunchy/MouseDriver/TuioMouse.java
@@ -24,86 +24,134 @@ package com.crunchy.MouseDriver;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Date;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import TUIO.*;
 
-public class TuioMouse implements TuioListener {
+public class TuioMouse implements TuioListener, Runnable {
 	
 	private Robot robot = null;
 	private int width = 0;
 	private int height = 0;
 	private long activeCuror = -1;
 	private MouseState currentState = MouseState.MouseState_Up;
-	private Date lastEvent;
-	
+	private LogLevel logLevel = LogLevel.LogLevel_Error;
+	private long lastEvent;
+	private boolean shouldRun = true;
+	private Date UpdateFPS = new Date();
 	public void addTuioObject(TuioObject tobj) {}
 	public void updateTuioObject(TuioObject tobj) {}	
 	public void removeTuioObject(TuioObject tobj) {}
 	public void refresh(TuioTime bundleTime) {}
+	Lock lock;
 	
 	public void addTuioCursor(TuioCursor tcur) {
-		
+		if (robot==null) return;
 		if(activeCuror != tcur.getCursorID()) {
-			if (robot!=null) robot.mouseMove(tcur.getScreenX(width),tcur.getScreenY(height));
-			if (robot!=null) robot.mouseRelease(InputEvent.BUTTON1_MASK);
+			robot.mouseMove(tcur.getScreenX(width),tcur.getScreenY(height));
+			robot.mouseRelease(InputEvent.BUTTON1_MASK);
 		}
 		
 		activeCuror = tcur.getCursorID();
-		if (robot!=null) 
+
+		robot.mouseMove(tcur.getScreenX(width),tcur.getScreenY(height));
+		robot.mousePress(InputEvent.BUTTON1_MASK);
+			
+		synchronized(this)
 		{
-			robot.mouseMove(tcur.getScreenX(width),tcur.getScreenY(height));
-			robot.mousePress(InputEvent.BUTTON1_MASK);
+			lastEvent = new Date().getTime();
 			currentState = MouseState.MouseState_Down;
-			lastEvent = new Date();
 		}
-		
 	}
 
 	public void updateTuioCursor(TuioCursor tcur) {
+		if (robot==null)return; 
 		if(activeCuror == tcur.getCursorID())
 		{
-			if (robot!=null) robot.mouseMove(tcur.getScreenX(width),tcur.getScreenY(height));
-			currentState = MouseState.MouseState_Move;
+			robot.mouseMove(tcur.getScreenX(width),tcur.getScreenY(height));
+			
 		}
 		else
 		{
-			if (robot!=null) robot.mouseMove(tcur.getScreenX(width),tcur.getScreenY(height));
-			if (robot!=null) robot.mouseRelease(InputEvent.BUTTON1_MASK);
+			robot.mouseMove(tcur.getScreenX(width),tcur.getScreenY(height));
+			robot.mouseRelease(InputEvent.BUTTON1_MASK);
 			
 			activeCuror = tcur.getCursorID();
-			if (robot!=null) 
-			{
-				robot.mouseMove(tcur.getScreenX(width),tcur.getScreenY(height));
-				robot.mousePress(InputEvent.BUTTON1_MASK);
-				currentState = MouseState.MouseState_Down;
-			}
+
+			robot.mouseMove(tcur.getScreenX(width),tcur.getScreenY(height));
+			robot.mousePress(InputEvent.BUTTON1_MASK);
+			
 		}
-		
-		lastEvent = new Date();
+		if(this.waitLock())
+		{
+			lastEvent = new Date().getTime();
+			currentState = MouseState.MouseState_Move;
+			lock.unlock();
+		}
 	}
 	
 	public void removeTuioCursor(TuioCursor tcur) {
+		if (robot==null) return;
 		if(activeCuror == tcur.getCursorID()) {
-			if (robot!=null) robot.mouseMove(tcur.getScreenX(width),tcur.getScreenY(height));
-			if (robot!=null) robot.mouseRelease(InputEvent.BUTTON1_MASK);
-			currentState = MouseState.MouseState_Up;
+			robot.mouseMove(tcur.getScreenX(width),tcur.getScreenY(height));
+			robot.mouseRelease(InputEvent.BUTTON1_MASK);
 			activeCuror = -1;
 			
-			lastEvent = new Date();
+			if(this.waitLock())
+			{
+				lastEvent = new Date().getTime();
+				currentState = MouseState.MouseState_Up;
+				lock.unlock();
+				System.out.println("Touch Up !");
+			}
 		}
+	}
+	
+	public boolean waitLock()
+	{
+		while(lock.tryLock() == false)
+		{
+			try {
+				Thread.sleep(1);
+			}catch(Exception except)
+			{
+				// dont care about exception
+			}
+		}
+		return true;
 	}
 	
 	public void Update()
 	{
-		if(currentState != MouseState.MouseState_Up)
+		Date current = new Date();
 		{
-			Date currentDate = new Date();
-			if (currentDate.getTime() - lastEvent.getTime() > 60)
+			
+			long diff = current.getTime() - UpdateFPS.getTime();
+			if(diff > 0 && logLevel == LogLevel.LogLevel_Verbose)
 			{
-				if (robot!=null) robot.mouseRelease(InputEvent.BUTTON1_MASK);
-				currentState = MouseState.MouseState_Up;
-				activeCuror = -1;
+				double fps = 1.0/((double)diff / 1000.0);
+				System.out.println("Update FPS is " + fps);
 			}
+			UpdateFPS = current;
+		}
+
+		if(this.waitLock())
+		{
+			if(currentState != MouseState.MouseState_Up)
+			{
+				if(logLevel == LogLevel.LogLevel_Verbose)System.out.println("LastEvent in Update = " + lastEvent);
+				long diff = current.getTime() - lastEvent;
+				if (diff > 133)
+				{
+					if (robot!=null) robot.mouseRelease(InputEvent.BUTTON1_MASK);
+					currentState = MouseState.MouseState_Up;
+					activeCuror = -1;
+					
+					if(logLevel == LogLevel.LogLevel_Info)System.out.println("LastEvent Timeout: diff = " + diff);
+				}
+			}
+			lock.unlock();
 		}
 	}
 	
@@ -115,9 +163,28 @@ public class TuioMouse implements TuioListener {
 			e.printStackTrace();
 			System.exit(0);
 		}
-		
+		lock = new ReentrantLock();
 		width  = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 		height = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+	}
+	
+	public static enum LogLevel
+	{
+		LogLevel_Error,
+		LogLevel_Info,
+		LogLevel_Verbose
+	};
+	
+	public static LogLevel parseLogLevel(String str)
+	{
+		if(str == "LogLevel_Error")
+			return LogLevel.LogLevel_Error;
+		else if(str == "LogLevel_Info")
+			return LogLevel.LogLevel_Info;
+		else if (str == "LogLevel_Verbose")
+			return LogLevel.LogLevel_Verbose;
+		else
+			throw new IllegalArgumentException("parseLogLevel error: argument must bus be one of LogLevel_Error , LogLevel_Info or LogLevel_Verbose");
 	}
 	
 	public static enum MouseState
@@ -130,17 +197,38 @@ public class TuioMouse implements TuioListener {
 	public static void main(String argv[]) {
 	
 		int port = 3333;
- 
+		LogLevel logLevel = LogLevel.LogLevel_Error;
 		if (argv.length==1) {
 			try { port = Integer.parseInt(argv[1]); }
-			catch (Exception e) { System.out.println("usage: java TuioMouse [port]"); }
+			catch (Exception e) { System.out.println("usage: java TuioMouse [port] [LogLevel_Info | LogLevel_Verbose]"); }
+		}
+		if(argv.length == 2)
+		{
+			try { 
+				port = Integer.parseInt(argv[1]); 
+				logLevel = TuioMouse.parseLogLevel(argv[2]);
+			}
+			catch (Exception e) { System.out.println("usage: java TuioMouse [port] [LogLevel_Info | LogLevel_Verbose]"); }
+		
 		}
 
  		TuioMouse mouse = new TuioMouse();
 		TuioClient client = new TuioClient(port);
 
+		mouse.logLevel = logLevel;
+		
+		Thread t = new Thread(mouse);
+		t.start();
 		System.out.println("listening to TUIO messages at port "+port);
 		client.addTuioListener(mouse);
 		client.connect();
+		
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(this.shouldRun) {
+			this.Update();
+		}
 	}
 }
